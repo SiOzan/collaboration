8913a864df0547aeebba622aac7c392c583ea819
SiOzan <fauzanhikmah12@gmail.com>
2024-09-12 10:01:41 +0700

    perubahan pada web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(
+    ['register' => false]
+);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
