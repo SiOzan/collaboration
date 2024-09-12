56e979c70d8e91f0baec7dff1bea615697560162
SiOzan <fauzanhikmah12@gmail.com>
2024-09-12 09:13:27 +0700

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::insert([
+            'name' => 'Paujan',
+            'email' => 'siozan@gmail.com',
+            'password' => Hash::make('12345678'),
+            'is_admin' => '1',
+        ]);
+
+        User::insert([
+            'name' => 'Alvin',
+            'email' => 'alvin@gmail.com',
+            'password' => Hash::make('12345678'),
+            'is_admin' => '1',
+        ]);
+    }
+}
