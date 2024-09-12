e646e750e61292e7263a347a84a2e15255371c01
alvin <alvinpurwo03@gmail.com>
2024-09-12 10:05:23 +0700

    perubahan pada web.php login

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes(
-    ['register' => false]
+    [
+        'register' => false,
+        'login' => false,
+    ]
 );
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
